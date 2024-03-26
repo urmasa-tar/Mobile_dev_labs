e29edaa333d794b08a73ca845012dbc57eedac64
urmasa-tar <nik.goltsev.97@bk.ru>
2024-03-26 14:09:08 +0700

    full_first_uplication

---
+++ Lab_01/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bt;
+    boolean fl = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        bt = findViewById(R.id.bt);
+        bt.setOnClickListener(this);
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View v) {
+        if(fl){
+            fl = false;
+            bt.setText("Pushed");
+        }else{
+            fl = true;
+            bt.setText("Push");
+
+        }
+    }
+}
