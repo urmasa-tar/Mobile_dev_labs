43b0e1af149d9285217953f7270caa6b413bd9db
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-09 14:40:23 +0300

    With_list_constructer

--- Lab_04/app/src/main/java/com/example/lab_04/MainActivity.java
+++ Lab_04/app/src/main/java/com/example/lab_04/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab_04;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,15 +11,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<Person> peopleList = new ArrayList();
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ListView mList = (ListView) findViewById(R.id.listView);
+
+        setInitialData();
+        ItemListAdapter adapter = new ItemListAdapter(this, R.layout.item_list,
+                peopleList);
+        mList.setAdapter(adapter);
+
+    }
+
+    private void setInitialData() {
+
+        peopleList.add(new Person("Чехов A. П.", "Писатель",
+                BitmapFactory.decodeResource(getResources(),
+                        R.drawable.chekhov)));
+        peopleList.add(new Person("Филатов Л. А", "Актер и писатель",
+                BitmapFactory.decodeResource(getResources(),
+                        R.drawable.filatov)));
+        peopleList.add(new Person("Матроскин", "Кот из Простоквашино",
+                BitmapFactory.decodeResource(getResources(),
+                        R.drawable.matroskin_2)));
+        peopleList.add(new Person("Пес","Из польского юмора",
+                BitmapFactory.decodeResource(getResources(),
+                        R.drawable.pes)));
+        peopleList.add(new Person("Ржевский","Герой анекдотов",
+                BitmapFactory.decodeResource(getResources(),
+                        R.drawable.poruchik)));
     }
 }
--- Lab_04/app/src/main/java/com/example/lab_04/itemListAdapter.java
+++ Lab_04/app/src/main/java/com/example/lab_04/itemListAdapter.java
@@ -1,10 +1,12 @@
 package com.example.lab_04;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
-
+import androidx.annotation.Nullable;
 
 
 class ItemListAdapter extends ArrayAdapter<Person> {
@@ -13,4 +15,28 @@ class ItemListAdapter extends ArrayAdapter<Person> {
     public ItemListAdapter(@NonNull Context context, int resource) {
         super(context, resource);
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView,
+                        @NonNull ViewGroup parent) {
+        // Сюда вставлять текст метода
+        String txtItem1 = getItem(position).getTitle();
+        String txtItem2 = getItem(position).getRef();
+        Bitmap imgItem = getItem(position).getImg();
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(mResource, parent, false);
+
+        TextView textView1 = (TextView) convertView.findViewById(R.id.titleView);
+        TextView textView2 = (TextView) convertView.findViewById(R.id.refView);
+        ImageView imgView = (ImageView) convertView.findViewById(R.id.imgView);
+
+        titleView.setText(txtItem1);
+        refView.setText(txtItem2);
+        imgView.setImageBitmap(imgItem);
+
+        return convertView;
+    }
+
 }
