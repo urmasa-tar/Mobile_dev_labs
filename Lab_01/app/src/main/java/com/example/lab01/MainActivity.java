5c89ef7cdb01fe437a9b67c86a56aaac6485b92a
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-05-06 06:03:07 +0300

    full_lab_01

--- Lab_01/app/src/main/java/com/example/lab01/MainActivity.java
+++ Lab_01/app/src/main/java/com/example/lab01/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab01;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,13 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button bt;
+    Button button2;
+    Button button3;
+    TextView txt;
+    String num;
+    int num_calc;
+
+
     boolean fl = true;
 
     @Override
@@ -27,13 +35,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        bt = (Button)findViewById(R.id.bt);
+        // Buttins and elements
+        bt = (Button) findViewById(R.id.bt);
         bt.setOnClickListener(this);
+
+        num_calc = 0;
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        txt = (TextView) findViewById(R.id.textView);
+        num = Integer.toString(num_calc);
+        button3.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
     }
 
+
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
+
+        txt.setText(Integer.toString(num_calc));
+
+        if (view.getId() == R.id.bt) {
+            // button1 action
+            bt.setText("Нажато");
+
+        } else if (view.getId() == R.id.button2) {
+            num_calc += 1;
+            txt.setText(Integer.toString(num_calc));
+            //button2 action
+        } else if (view.getId() == R.id.button3) {
+            //button3 action
+            num_calc = 0;
+            txt.setText(Integer.toString(num_calc));
+        }
 
     }
 }
