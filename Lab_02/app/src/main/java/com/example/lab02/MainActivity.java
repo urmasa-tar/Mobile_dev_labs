4fa56ed6321ef38f653b2ea6ebe9ef51660dfb28
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-05-06 12:38:57 +0300

    end_of_proj02

--- Lab_02/app/src/main/java/com/example/lab02/MainActivity.java
+++ Lab_02/app/src/main/java/com/example/lab02/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab02;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void onClick(View view) {
+
+            mediaPlayer = MediaPlayer.create(getApplicationContext(),
+                    R.raw.dog_sound);
+            mediaPlayer.start();
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Покорми пса", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 0);
+            toast.show();
+
+    }
+
+    public void catClick(View view) {
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),
+                R.raw.cat_sound);
+        mediaPlayer.start();
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Покорми кота", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.show();
+
     }
 }
