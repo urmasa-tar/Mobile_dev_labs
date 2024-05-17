7836c3ae152ebb7335a444f1b5e8a23e3b15de55
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-05-17 15:15:56 +0300

    full_pi_num

--- Lab_03/app/src/main/java/com/example/pi_number/MainActivity.java
+++ Lab_03/app/src/main/java/com/example/pi_number/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pi_number;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +13,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tvOut;
+    String Pi_str = "";
+    int Pi_num = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        tvOut = (TextView) findViewById(R.id.txOut);
+        btn = (Button) findViewById(R.id.button);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +31,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+
+    public static String piSpigot(final int n) {
+        // найденные цифры сразу же будем записывать в StringBuilder
+        StringBuilder pi = new StringBuilder(n);
+        int boxes = n * 10 / 3;	// размер массива
+        int reminders[] = new int[boxes];
+        // инициализируем масив двойками
+        for (int i = 0; i < boxes; i++) {
+            reminders[i] = 2;
+        }
+        int heldDigits = 0;    // счётчик временно недействительных цифр
+        for (int i = 0; i < n; i++) {
+            int carriedOver = 0;    // перенос на следующий шаг
+            int sum = 0;
+            for (int j = boxes - 1; j >= 0; j--) {
+                reminders[j] *= 10;
+                sum = reminders[j] + carriedOver;
+                int quotient = sum / (j * 2 + 1);   // результат деления суммы на знаменатель
+                reminders[j] = sum % (j * 2 + 1);   // остаток от деления суммы на знаменатель
+                carriedOver = quotient * j;   // j - числитель
+            }
+            reminders[0] = sum % 10;
+            int q = sum / 10;	// новая цифра числа Пи
+            // регулировка недействительных цифр
+            if (q == 9) {
+                heldDigits++;
+            } else if (q == 10) {
+                q = 0;
+                for (int k = 1; k <= heldDigits; k++) {
+                    int replaced = Integer.parseInt(pi.substring(i - k, i - k + 1));
+                    if (replaced == 9) {
+                        replaced = 0;
+                    } else {
+                        replaced++;
+                    }
+                    pi.deleteCharAt(i - k);
+                    pi.insert(i - k, replaced);
+                }
+                heldDigits = 1;
+            } else {
+                heldDigits = 1;
+            }
+            pi.append(q);	// сохраняем найденную цифру
+        }
+        if (pi.length() >= 2) {
+            pi.insert(1, '.');	// добавляем в строчку точку после 3
+        }
+        return pi.toString();
+    }
+
+    public void Button_Click(View view) {
+        ((TextView)findViewById(R.id.txOut)).setText(piSpigot(Pi_num));
+        Pi_num += 1;
     }
 }
