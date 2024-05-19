4cc377e3bcb371dc60420549be66c2b5fbf562a3
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-05-19 22:54:28 +0300

    Animals_list

--- Lab_04/app/src/main/java/com/example/lists_lab04/MainActivity.java
+++ Lab_04/app/src/main/java/com/example/lists_lab04/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lists_lab04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     String[] arr_of_str;
 
+    ArrayList<String> animals = new ArrayList();// Массив строк
+    ArrayList<String> selectedAnimal = new ArrayList();// Выбранные строки
+    ArrayAdapter<String> adapter;// Адаптер для связи массива с ListView
+    ListView animList;
 
 
     @Override
@@ -24,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        animals.add("Кошки");
+        animals.add("Собаки");
+        animals.add("Хомяки");
+        animals.add("Еноты");
+        animals.add("Черепахи");
+
+        // Закрепление имени за элементом
+        animList = (ListView) findViewById(R.id.animList);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice, animals);
+        animList.setAdapter(adapter);
+
+    }
+
+    public void OnClickAdd(View view) {
+    }
+
+    public void OnClickDel(View view) {
     }
 }
